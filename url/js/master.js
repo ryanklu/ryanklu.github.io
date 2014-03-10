480c6d5d50dcf86bcfa52d3aa51351c8ea47e592
Ryan Lu <rlu@webmd.net>
2014-03-10 15:48:37 -0400

    objective literaled the js

--- url/js/master.js
+++ url/js/master.js
@@ -1,113 +1,102 @@
 ryanklu = {};
 ryanklu.url = {
 
+	input : $('input'),
+	submitBtn : $('button.submit'),
+	resetBtn : $('button.clear'),
+	pcBtn : $('#copy-pc-button'),
+	macBtn : $('#copy-mac-button'),
+	pcMacBtn : $('#copy-mac-button, #copy-pc-button'),
+	pcInput : $('input.pcUrl'),
+	macInput : $('input.macUrl'),
+
 	init : function(){
-		//console.log('init');
-		var _self =  this,
-			submitBtn = $('button.submit'),
-			input = $('input'),
-			resetBtn = $('button.clear');
+		var $self =  this,
+			$object = ryanklu.url;
 
-		_self.copyUrl();
-		_self.clearPage();
+		$self.copyUrl();
+		$self.clearPage();
 
-		submitBtn.click(function(){
-			_self.generateUrl();
+		$object.submitBtn.click(function(){
+			$self.generateUrl();
 		});
-		input.keypress(function (e) {
+		$object.input.keypress(function (e) {
 			if (e.which == 13) {
-				_self.generateUrl();
+				$self.generateUrl();
 				e.preventDefault();
 			}
 		});
-		resetBtn.click(function(){
-			_self.resetButton();
+		$object.resetBtn.click(function(){
+			$self.resetButton();
 		});
 	},
 
 	clearPage : function(){
-		//console.log('clearPage');
-		var _self =  this,
-			input = $('input'),
-			pcBtn = $('#copy-pc-button'),
-			macBtn = $('#copy-mac-button'),
-			pcMacBtn = $('#copy-mac-button, #copy-pc-button');
+		var $self =  this,
+			$object = ryanklu.url;
 
-		input.not(':button, :submit, :reset, :hidden').val('');
-		pcBtn.attr('data-clipboard-text', '');
-		macBtn.attr('data-clipboard-text', '');
-		pcMacBtn.removeClass('copied').find('i').attr('class','icon-plus-sign');
+		$object.input.not(':button, :submit, :reset, :hidden').val('');
+		$object.pcBtn.attr('data-clipboard-text', '');
+		$object.macBtn.attr('data-clipboard-text', '');
+		$object.pcMacBtn.removeClass('copied').find('i').attr('class','icon-plus-sign');
 	},
 
 	generatePcUrl : function(){
-		//console.log('generating pc url');
-		var _self =  this,
-			pcUrl = $('input.macUrl').val(),
-			pcUrlGen = pcUrl.replace("smb:","").replace(/\//g, "\\"),
-			pcInput = $('input.pcUrl'),
-			pcBtn = $('#copy-pc-button'),
-			macBtn = $('#copy-mac-button'),
-			submitBtn = $('button.submit');
+		var $self =  this,
+			$object = ryanklu.url;
+			$pcUrl = $('input.macUrl').val(),
+			$pcUrlGen = $pcUrl.replace("smb:","").replace(/\//g, "\\");
 
-		pcInput.val(pcUrlGen);
-		pcBtn.attr('data-clipboard-text', pcUrlGen);
-		macBtn.attr('data-clipboard-text', pcUrl);
+		$object.pcInput.val($pcUrlGen);
+		$object.pcBtn.attr('data-clipboard-text', $pcUrlGen);
+		$object.macBtn.attr('data-clipboard-text', $pcUrl);
 		setTimeout(function () {
-			submitBtn.button('reset')
+			$object.submitBtn.button('reset')
         }, 400);
 	},
 
 	generateMacUrl : function(){
-		//console.log('generating mac url');
-		var _self =  this,
-			macUrl = $('input.pcUrl').val(),
-			macUrlGen = macUrl.replace('\\\\','smb:\\\\').replace(/\\/g, '/'),
-			macInput = $('input.macUrl'),
-			pcBtn = $('#copy-pc-button'),
-			macBtn = $('#copy-mac-button'),
-			submitBtn = $('button.submit');
+		var $self =  this,
+			$object = ryanklu.url;
+			$macUrl = $('input.pcUrl').val(),
+			$macUrlGen = $macUrl.replace('\\\\','smb:\\\\').replace(/\\/g, '/');
 
-		macInput.val(macUrlGen);
-		macBtn.attr('data-clipboard-text', macUrlGen);
-		pcBtn.attr('data-clipboard-text', macUrl);
+		$object.macInput.val($macUrlGen);
+		$object.macBtn.attr('data-clipboard-text', $macUrlGen);
+		$object.pcBtn.attr('data-clipboard-text', $macUrl);
 		setTimeout(function () {
-			submitBtn.button('reset')
+			$object.submitBtn.button('reset')
         }, 400);
 	},
 
 	generateUrl : function(){
-		//console.log('generating url')
-		var _self =  this,
-			macInput = $('input.macUrl'),
-			pcInput = $('input.pcUrl'),
-			submitBtn = $('button.submit');
+		var $self =  this,
+			$object = ryanklu.url;
 
-		if (macInput.val()){
-			submitBtn.button('loading');
-			_self.generatePcUrl();
-		} else if (pcInput.val()){
-			submitBtn.button('loading');
-			_self.generateMacUrl();
+		if ($object.macInput.val()){
+			$object.submitBtn.button('loading');
+			$self.generatePcUrl();
+		} else if ($object.pcInput.val()){
+			$object.submitBtn.button('loading');
+			$self.generateMacUrl();
 		}
 	},
 
 	resetButton : function(){
-		//console.log('reset');
-		var _self =  this;
+		var $self =  this;
 
-		_self.clearPage();
+		$self.clearPage();
 	},
 
 	copyUrl : function(){
-		var _self =  this,
+		var $self =  this,
+			$object = ryanklu.url,
 			client = new ZeroClipboard($('#copy-mac-button, #copy-pc-button'));
 
 		client.on( "load", function(client) {
-			//console.log('movie has loaded');
 			client.on( "complete", function(client, args) {
-				//console.log('copying is complete');
-				if($('input.pcUrl').val()){
-					$('#copy-mac-button, #copy-pc-button').removeClass('copied').find('i').attr('class','icon-plus-sign');
+				if($object.pcInput.val()){
+					$object.pcMacBtn.removeClass('copied').find('i').attr('class','icon-plus-sign');
 					$(this).addClass('copied').find('i').attr('class','icon-ok-sign');
 				}
 			});
